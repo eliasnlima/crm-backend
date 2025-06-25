79e6a25559e6f2c67e5530823e6612cf654abe06
elias <eliasnerylima018@gmail.com>
2025-06-24 22:05:12 -0300

    add delete, show, uptade a Client

--- controllers/ClientController.js
+++ controllers/ClientController.js
@@ -1,6 +1,17 @@
 import Client from "../models/Client.js"
+import Action from "../models/Action.js"
 
 class ClientController{
+
+    async show(req, res){
+        
+        const { userId } = req.params
+
+        const clients = await Client.find({ user: userId})
+
+        return res.json({clients})
+
+    }
 
     async store(req, res){
 
@@ -11,6 +22,31 @@ class ClientController{
 
         return res.status(201).json({ message: "Cliente cadastrado com sucesso", client})
 
+    }
+
+    async delete(req, res){
+
+        const { clientId } = req.body
+
+
+        const action = await Action.deleteMany({ client: clientId})
+        const client = await Client.findByIdAndDelete({ _id: clientId })
+
+        return res.json({ message: "Cliente removido com sucesso!"})
+
+    }
+
+    async update(req, res){
+
+        const { clientId } = req.params
+        const { nome, CNPJ } = req.body
+
+        const client = await Client.updateOne({ _id: clientId}, {
+            nome,
+            CNPJ
+        })
+
+        return res.json({ message: "Atualizado!"})
     }
 }
 
